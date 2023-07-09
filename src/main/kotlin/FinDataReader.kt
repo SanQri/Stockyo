ce87435ec8830c610e15309ca7000b35bb9086c2
SanQri <galiullinnikolai@gmail.com>
2023-07-08 19:12:33 -0700

    Initial commit 2

---
+++ src/main/kotlin/FinDataReader.kt
@@ -0,0 +1,16 @@
+import java.io.File
+
+class FinDataReader(path: String) {
+
+    private val file: File
+
+    init {
+        this.file = File(path)
+    }
+
+    fun makeData() {
+
+    }
+
+}
+
